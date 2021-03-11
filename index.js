2b4dc0dce19091ccf80e23e93e96b3d604a9eb14
agoomar <agoomar1996@gmail.com>
2021-03-11 12:20:56 -0600

    created react-app

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
-
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname)))
 
 console.log(process.env.NODE_ENV)
 
